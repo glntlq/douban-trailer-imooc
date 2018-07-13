9b90e309ae247f7bedb3ff2aef6fdb936d5b52bf
gln <1368807467@qq.com>
2018-07-13 11:23:48 +0800

    feat: 新增模板引擎

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const Koa = require('koa')
-const { normal } = require('./tepl')
+const { htmlTpl, ejsTpl, pugTpl } = require('./tepl')
+const ejs = require('ejs')
+const pug = require('pug')
 
 const app = new Koa()
 app.use(async (ctx, next) => {
     ctx.type = 'text/html; charset=utf-8'
-    ctx.body = normal
+    ctx.body = pug.render(pugTpl, {
+        you: 'Luke',
+        me: 'Scotte'
+    })
 })
 
 app.listen(4455)
